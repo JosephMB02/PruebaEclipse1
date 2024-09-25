37982511d19bc77ddf00f4b9930a269e73ace145
josep <josephmiguel@gmail.com>
2024-09-25 17:45:02 -0600

    Agregadas variables

--- src/EjemploGIT.java
+++ src/EjemploGIT.java
@@ -3,6 +3,7 @@ public class EjemploGIT {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
         System.out.print("Hola alumnos de GIT");
+        String nombre = "Joseph Miguel Barrero Quiñones";
 	}
 
 }
